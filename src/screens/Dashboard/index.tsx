1b43e0035bd1e8f1dc9f5527f9296e78d4577bd4
Flávio Dutra <flaviomdutra@outlook.com>
2022-12-20 17:44:34 -0300

    Initial commit

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import { HighlightCard } from "../../components/HighlightCard";
+
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+  HighlightCards,
+} from "./styles";
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: "https://github.com/flaviomdutra.png" }} />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Flávio</UserName>
+            </User>
+          </UserInfo>
+
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+
+      <HighlightCards>
+        <HighlightCard />
+        <HighlightCard />
+        <HighlightCard />
+      </HighlightCards>
+    </Container>
+  );
+}
