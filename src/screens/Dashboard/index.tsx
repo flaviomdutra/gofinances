2c7a70b33cc73217a350d1121f0bf349ed177200
Flávio Dutra <flaviomdutra@outlook.com>
2022-12-20 18:24:25 -0300

    style of highlight cards

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -33,9 +33,24 @@ export function Dashboard() {
       </Header>
 
       <HighlightCards>
-        <HighlightCard />
-        <HighlightCard />
-        <HighlightCard />
+        <HighlightCard
+          type="up"
+          title="Entradas"
+          amount="R$ 17.400,00"
+          lastTransaction="Última entrada dia 13 de abril"
+        />
+        <HighlightCard
+          type="down"
+          title="Saídas"
+          amount="R$ 1.2359,00"
+          lastTransaction="Última entrada dia 03 de abril"
+        />
+        <HighlightCard
+          type="total"
+          title="Total"
+          amount="R$ 17.400,00"
+          lastTransaction="01 à 18 de abril"
+        />
       </HighlightCards>
     </Container>
   );
