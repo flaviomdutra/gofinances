c1d32c211255a95b25ff452c8323ef103b860564
Flávio Dutra <flaviomdutra@outlook.com>
2023-01-13 23:03:38 -0300

    feat: SignIn page

--- App.tsx
+++ App.tsx
@@ -16,8 +16,9 @@ import theme from "./src/global/styles/theme";
 
 import { NavigationContainer } from "@react-navigation/native";
 
-import { AppRoutes } from "./src/routes/app.routes";
 import { StatusBar } from "react-native";
+import { SignIn } from "./src/screens/SignIn";
+import { AuthProvider } from "./src/hooks/auth";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -34,7 +35,9 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <NavigationContainer>
         <StatusBar barStyle="light-content" />
-        <AppRoutes />
+        <AuthProvider>
+          <SignIn />
+        </AuthProvider>
       </NavigationContainer>
     </ThemeProvider>
   );
